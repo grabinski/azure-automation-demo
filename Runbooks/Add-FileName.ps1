e3a9ff02e90101ff5a946f46e1240d7670ab2836
Jim Grabinski <grabinski@users.noreply.github.com>
2019-06-27 19:22:46 -0400

    Fix variable evaluation

--- Runbooks/Add-FileName.ps1
+++ Runbooks/Add-FileName.ps1
@@ -3,10 +3,10 @@ param(
 )
 
 $data = $EVENTDATA.EventProperties.Data | ConvertFrom-Json
-Write-Verbose $data
+$data
 
 $cred = Get-AutomationPSCredential -Name 'Demo-SP-Site-Cred'
 $url = Get-AutomationVariable -Name 'Demo-SP-Site-URL'
 
 $siteConnection = Connect-PnPOnline -Credentials $cred -Url $url -ReturnConnection
-$addedItem = Add-PnPListItem -List 'File Names' -Values @{ 'Title' = "$data.FileName" } -Connection $siteConnection
+$addedItem = Add-PnPListItem -List 'File Names' -Values @{ 'Title' = "$($data.FileName)" } -Connection $siteConnection
