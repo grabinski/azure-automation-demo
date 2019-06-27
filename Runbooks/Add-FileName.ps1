6270f7a44ac4003b27f460096c94a3c6e71b7dac
Jim Grabinski <grabinski@users.noreply.github.com>
2019-06-27 17:20:31 -0400

    Create initial add filename script

---
+++ Runbooks/Add-FileName.ps1
@@ -0,0 +1,4 @@
+$cred = Get-AutomationPSCredential -Name 'Demo-SP-Site-Cred'
+$url = Get-AutomationVariable -Name 'Demo-SP-Site-URL'
+$siteConnection = Connect-PnPOnline -Credentials $cred -Url $url -ReturnConnection
+$addedItem = Add-PnPListItem -List 'File Names' -Values @{ 'Title' = 'Test Title' } -Connection $siteConnection
