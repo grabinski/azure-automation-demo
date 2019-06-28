0eca650229b6719d64a7dda176c2ba6390b59327
Jim Grabinski <grabinski@users.noreply.github.com>
2019-06-27 20:13:25 -0400

    Remove path

--- Runbooks/Add-FileName.ps1
+++ Runbooks/Add-FileName.ps1
@@ -4,9 +4,10 @@ param(
 
 $data = $EVENTDATA.EventProperties.Data | ConvertFrom-Json
 $data
+$fileName = $data.FileName -replace '^.*\', ''
 
 $cred = Get-AutomationPSCredential -Name 'Demo-SP-Site-Cred'
 $url = Get-AutomationVariable -Name 'Demo-SP-Site-URL'
 
 $siteConnection = Connect-PnPOnline -Credentials $cred -Url $url -ReturnConnection
-$addedItem = Add-PnPListItem -List 'File Names' -Values @{ 'Title' = "$($data.FileName)" } -Connection $siteConnection
+$addedItem = Add-PnPListItem -List 'File Names' -Values @{ 'Title' = "$fileName" } -Connection $siteConnection
